ce7716630c1dbc2680516fa36d5314058d2e7a73
Bull1016 <octavel679@gmail.com>
2026-06-05 02:00:02 +0100

    chore: update footer copyright year to dynamic date

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
 
     <footer class="bg-gray-100 py-6 mt-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-gray-600">
-            <p>&copy; 2024 Texts to Book. Transform your ideas into beautiful books.</p>
+            <p>&copy; {{ date('Y') }} Texts to Book. Transform your ideas into beautiful books.</p>
         </div>
     </footer>
 </body>
